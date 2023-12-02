ce9b1792e35e8baf88d3b9ab0e6c768f39ca178f
Aadhil Ahamed <aadhil2001ahamed@gmail.com>
2023-12-02 18:56:50 +0530

    Initial commit

---
+++ app/src/main/java/com/aadhil/cineworlddigital/Login.java
@@ -0,0 +1,44 @@
+package com.aadhil.cineworlddigital;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class Login extends Fragment {
+    public Login() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View fragment, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(fragment, savedInstanceState);
+
+        Button button = fragment.findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity mainActivity = (MainActivity) getActivity();
+                mainActivity.setFragment(MainActivity.REGISTER_FRAGMENT);
+            }
+        });
+    }
+}
